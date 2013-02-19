752d1086cfdcb4d367ecb74644047f1a4370118b
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2013-02-19 20:27:28 +0100

    added autogen_generated_stuff* scripts

---
+++ autogen_generated_stuff_overwrite.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+[ -d ../libpolys ] || { echo "the subdirectory libpolys does not exist in the parent directory; this does not seem to be the directory Singular/AUTOGEN_generated_stuff"; exit 1;}
+
+. autogen_generated_stuff_data
+
+cd ..
+
+[ -d "${DIR_autogen}" ] || { echo "the subdirectory ${DIR_autogen}; please invoke the script autogen_generated_stuff_clone.sh in the Singular root directory"; exit 1;}
+
+for file in ${autogen_generated_stuff}; do
+    if [ ! -L "${file}" ]; then
+	mv -vf "${file}" "${DIR_autogen}/${file}"
+	ln -snf "${DIR_autogen}/${file}" "${file}"
+    fi;
+done
